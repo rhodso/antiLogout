2f2c010a92056f4cd3bd7ba95967d23960a763f1
rhodso <rhodsoprivate@gmail.com>
2022-02-09 09:07:30 +0000

    Changed the way teams is killed

--- antiLogout.py
+++ antiLogout.py
@@ -15,18 +15,35 @@ except:
 #Startup
 print("Started")
 
-#Wait some seconds
-time.sleep(10)
-os.system("taskkill /IM teams.exe /F")
-print("Killed teams lol")
+print("")
+loopFlag = True
+for loopCount in range(60, -1, -1):
+    status = os.system("taskkill /IM teams.exe /F")
+    if(status == 0):
+        print("\nKilled teams")
+        loopFlag = False
+        break
+    elif(status == 128):
+        print("\nTeams not running, continuing")
+        loopFlag = False
+        break
+    else:
+        print("\rTeams could not be killed, trying again " + str(loopCount) + " more times: ", end="")
+        time.sleep(1)
 
+if(loopFlag):
+    print("\nTried to kill teams 60 times, but teams is still running. Gave up trying to kill teams")
+
+keypresses = 0
 keyboard = Controller()
 while(True):
     try:
-        print("Pressed")
+        keypresses += 1
+        print("\rPressed " + str(keypresses) + " time(s)", end="")
         keyboard.tap(Key.f14)
-        time.sleep(300)
+        time.sleep(60)
     except:
         break
-print("Stopped")
+print("\nStopped")
 anyKey()
+
